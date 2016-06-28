7e3f3104dd9633429067c043a0eb8a6f5ed3f272
Anderson Ivan Witzke <anderson@websix.com.br>
2016-06-28 14:02:16 -0300

    Fixed namespace

--- lib/Agendor/Model.php
+++ lib/Agendor/Model.php
@@ -1,5 +1,5 @@
 <?php
-namespace W6\Agendor;
+namespace Agendor;
 
 class AgendorModel extends AgendorObject
 {
--- lib/Agendor/People.php
+++ lib/Agendor/People.php
@@ -1,5 +1,5 @@
 <?php
-namespace W6\Agendor;
+namespace Agendor;
 
 class AgendorPeople extends AgendorModel
 {
