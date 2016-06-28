391f1fb5f5682d3ed73088bdaf466c795f00777a
Anderson Ivan Witzke <anderson@websix.com.br>
2016-06-28 16:46:05 -0300

    Adicionado set e get de ids corrigidos

--- lib/Agendor/Deal.php
+++ lib/Agendor/Deal.php
@@ -2,4 +2,18 @@
 
 class AgendorDeal extends AgendorModel
 {
+    public function setId($value)
+    {
+        $this->_attributes["dealId"] = $value;
+        $this->_unsavedAttributes->add("dealId");
+    }
+
+    public function getId()
+    {
+        if (array_key_exists("dealId", $this->_attributes)) {
+            return $this->_attributes["dealId"];
+        } else {
+            return null;
+        }
+    }
 }
--- lib/Agendor/People.php
+++ lib/Agendor/People.php
@@ -2,4 +2,18 @@
 
 class AgendorPeople extends AgendorModel
 {
+    public function setId($value)
+    {
+        $this->_attributes["personId"] = $value;
+        $this->_unsavedAttributes->add("personId");
+    }
+
+    public function getId()
+    {
+        if (array_key_exists("personId", $this->_attributes)) {
+            return $this->_attributes["personId"];
+        } else {
+            return null;
+        }
+    }
 }
